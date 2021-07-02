623d08677eeb0d8c1153d784eacbf15b25d1a313
jhwillett <jhw@prosperworks.com>
2021-07-02 14:23:38 -0700

    Do not use this branch!  Use main instead.

--- lib/redis/script_manager/version.rb
+++
@@ -1,5 +0,0 @@
-class Redis
-  class ScriptManager
-    VERSION = '0.0.6'.freeze
-  end
-end
--- test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'redis/script_manager'
-require 'minitest/autorun'
-require 'redis'
-require 'redis/namespace'
